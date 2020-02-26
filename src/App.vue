f779f4fc10cade566b6b15b1edb5ad8e480c5d6c
Dieter Evangelista <hansevangelista@gmail.com>
2020-02-26 18:55:13 -0500

    add change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <section class="home"> <br><br><br>
+    <section class="home" v-show="page == 'home'"> <br><br><br>
 
       <img class="logo" src="https://lh3.googleusercontent.com/proxy/Lz76GvNGhMEq8ngBZAETByagoHiC8upboleTsYcuXzjzkjP_aT-w93BaOPRWXHSp_8_GzAXCua9xTHDp_IMj52y4jE8namQJnwAKeUEeZZmo7WGZSpQSLPXgVA"> <br><br>
 
@@ -16,6 +16,23 @@
 
     </section>
 
+    <section class="details" v-show="page == 'details'">
+      <header>
+        <img class="logo" src="https://lh3.googleusercontent.com/proxy/Lz76GvNGhMEq8ngBZAETByagoHiC8upboleTsYcuXzjzkjP_aT-w93BaOPRWXHSp_8_GzAXCua9xTHDp_IMj52y4jE8namQJnwAKeUEeZZmo7WGZSpQSLPXgVA">
+      </header>
+      <section>
+        <p>SKU: 123456789ABCD</p>
+        <p>10 huevos AA</p>
+
+        <p>Caja: 12 SKUs</p>
+        <p>Pallet: 120 Cajas</p> <br><br>
+
+        <img src="https://es.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/core_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"> <br><br>
+
+        <button>Ingresar en almacén</button>
+      </section>
+    </section>
+
   </div>
 </template>
 
@@ -25,34 +42,32 @@ export default {
   name: 'App',
   data: () => {
     return {
-      text: 'Vue.js'
+      page: 'home',
+      // page: 'details',
+      value: '',
     }
   },
   mounted: () => {
-    console.log('scan ...')
+    // console.log('scan ...')
   },
   methods: {
     openQRCamera: function (event) {
       let node = event.target
-      console.log('node: ', node)
-
       let self = this
-
-      var reader = new FileReader();
+      var reader = new FileReader()
       reader.onload = function() {
-        node.value = "";
         qrcode.callback = function(res) {
           if(res instanceof Error) {
-            alert("Asegúrese de que el código QR esté dentro del marco de la cámara e intente nuevamente.");
+            alert("Asegúrese de que el código QR esté dentro del marco de la cámara e intente nuevamente.")
           } else {
-            console.log('res: ', res)
-            self.text = res
-            alert(":)" + self.text);
+            // console.log('res: ', res)
+            self.value = res
+            self.page = 'details'
           }
         };
-        qrcode.decode(reader.result);
+        qrcode.decode(reader.result)
       };
-      reader.readAsDataURL(node.files[0]);
+      reader.readAsDataURL(node.files[0])
     }
   }
 }
@@ -63,25 +78,55 @@ export default {
   body
   p
     margin 0
+  button
+    outline 0
+    border 0
 
-  /* code */
+  /* general */
   body
-    background #30da7b
     font-family 'Arial'
 
-  .logo
-    max-width 150px
-    margin auto
-    display table
-  .qr
-    margin auto
-    display table
+  /* home */
+  .home
+    background #30da7b
+    height 100vh
+    .logo
+      max-width 150px
+      margin auto
+      display table
+    .qr
+      margin auto
+      display table
+    p
+      text-align center
+      color #eee
+      font-size 20px
+      b
+        color #fff
 
-  p
-    text-align center
-    color #eee
-    font-size 20px
-    b
-      color #fff
+  /* details */
+  .details
+    header
+      background #30da7b
+      padding 8px 16px
+      img
+        width 100px
+    section
+      padding 16px
+      p
+        margin 8px 0
+      img
+        max-width 240px
+        margin auto
+        display table
+      button
+        background #30da7b
+        border-radius 4px
+        font-family 'Arial'
+        color white
+        font-size 16px
+        padding 12px 16px
+        display table
+        margin auto
 
 </style>
