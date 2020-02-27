7f16300b8b3cb2b6cedc9612c86582ea97fa1683
Dieter Evangelista <hansevangelista@gmail.com>
2020-02-27 09:01:23 -0500

    new details

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,10 @@
         <img class="logo" src="https://lh3.googleusercontent.com/proxy/Lz76GvNGhMEq8ngBZAETByagoHiC8upboleTsYcuXzjzkjP_aT-w93BaOPRWXHSp_8_GzAXCua9xTHDp_IMj52y4jE8namQJnwAKeUEeZZmo7WGZSpQSLPXgVA">
       </header>
       <section>
-        <p>SKU: 7707304621360</p>
-        <p>Descripción: Plus AA x 12 unid</p>
-        <p>Embalaje: Caja x 12 7707304621360A</p>
-        <p>Pallet: 12 cajas (1728 unid)</p> <br>
+        <p>SKU: <span>7707304621360</span></p>
+        <p>Descripción: <span>Plus AA x 12 unid</span></p>
+        <p>Embalaje: <span>Caja x 12 (7707304621360A)</span></p>
+        <p>Pallet: <span>12 cajas (1728 unid)</span></p> <br>
 
         <img src="https://es.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/core_market/generator/dist/generator/assets/images/websiteQRCode_noFrame.png"> <br>
 
@@ -118,6 +118,9 @@ export default {
       padding 16px
       p
         margin 8px 0
+        span
+          color #555
+          font-size 15px
       img
         max-width 240px
         margin auto
